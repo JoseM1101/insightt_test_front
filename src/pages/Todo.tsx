ad6d1c8cefe2cfee94e0fbbbf462cb4655432f40
Jose Molina <josemolina110101@gmail.com>
2025-12-20 14:31:37 -0400

    add logout button to todo list

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -6,17 +6,25 @@ import {
   Button,
   Paper,
   TextField,
+  List,
+  IconButton,
+  Tooltip,
 } from "@mui/material"
+import TodoItem from "../components/TodoItem"
 import AddIcon from "@mui/icons-material/Add"
-import { samples } from "../tasks"
-import TodoList from "../components/TodoList"
-import type { Task } from "../types"
+import LogoutIcon from "@mui/icons-material/Logout"
 import Modal from "../components/Modal"
+import { useTasks } from "../hooks/useTasks"
+import { useAuth } from "../hooks/useAuth"
+import { useNavigate } from "react-router-dom"
 
 export default function TodoPage() {
-  const [tasks, setTasks] = useState(samples)
+  const { tasks, createTask, updateTask, deleteTask, toggleCompleted } =
+    useTasks()
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [newTaskTitle, setNewTaskTitle] = useState("")
+  const navigate = useNavigate()
+  const { authLogout } = useAuth()
 
   const handleAddClick = () => setIsModalOpen(true)
 
@@ -28,9 +36,14 @@ export default function TodoPage() {
   const handleConfirmAdd = () => {
     if (newTaskTitle.trim() === "") return
     const newTask = { title: newTaskTitle, completed: false }
-    setTasks([...tasks, newTask as Task])
+    createTask(newTask)
     setNewTaskTitle("")
     setIsModalOpen(false)
+  }
+
+  const handleLogout = () => {
+    authLogout()
+    navigate("/")
   }
 
   return (
@@ -42,17 +55,42 @@ export default function TodoPage() {
         mb={3}
       >
         <Typography variant="h5">My Tasks</Typography>
-        <Button
-          variant="contained"
-          startIcon={<AddIcon />}
-          onClick={handleAddClick}
-        >
-          Add Task
-        </Button>
+
+        <Box display="flex" gap={2} alignItems="center">
+          <Button
+            variant="contained"
+            startIcon={<AddIcon />}
+            onClick={handleAddClick}
+          >
+            Add Task
+          </Button>
+
+          <Tooltip title="Logout" sx={{ px: 0 }}>
+            <IconButton onClick={handleLogout}>
+              <LogoutIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
       </Box>
 
       <Paper elevation={2}>
-        <TodoList tasks={tasks} />
+        <List sx={{ py: 0 }}>
+          {tasks.length > 0 ? (
+            tasks.map((task) => (
+              <TodoItem
+                key={task.id}
+                task={task}
+                onUpdate={updateTask}
+                onDelete={deleteTask}
+                onToggle={toggleCompleted}
+              />
+            ))
+          ) : (
+            <Typography variant="body1" sx={{ p: 2 }}>
+              No tasks to display.
+            </Typography>
+          )}
+        </List>
       </Paper>
 
       <Modal
