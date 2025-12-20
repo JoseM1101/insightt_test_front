a891930c00766a2228b4096c82c215678dfaf590
Jose Molina <josemolina110101@gmail.com>
2025-12-20 14:31:55 -0400

    implement context providers

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,18 @@ import { createRoot } from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes.tsx"
 import { CssBaseline } from "@mui/material"
+import { AuthProvider } from "./context/AuthContext"
+import { LoadingProvider } from "./context/LoadingContext.tsx"
+import LoaderOverlay from "./components/LoaderOverlay.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CssBaseline />
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <LoadingProvider>
+        <CssBaseline />
+        <RouterProvider router={router} />
+        <LoaderOverlay />
+      </LoadingProvider>
+    </AuthProvider>
   </StrictMode>
 )
